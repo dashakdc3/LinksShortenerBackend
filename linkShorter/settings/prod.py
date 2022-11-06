eac26b745d1cad26e0c4a0aeacf6acfa4b91e6bf
dashakdc3 <dashakdc3@mail.ru>
2022-11-06 14:22:37 +0300

    Update frontend data

--- linkShorter/settings/prod.py
+++ linkShorter/settings/prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ["vfls.herokuapp.com"]
+ALLOWED_HOSTS = ["vfls.herokuapp.com", "f-links-shortener.herokuapp.com"]
 
 DATABASES = {
     'default': dj_database_url.config()
